0f4f34866f747c0b5945e5633315ed89b92547e6
sharath <sharathdasari1719@gmail.com>
2026-03-04 16:57:11 +0530

    Maximum Subarray using kadane's algorithm

--- Maximum Subarray.java
+++ Maximum Subarray.java
@@ -12,6 +12,21 @@ class Solution {
     }
 }
 
+//--------kadane algorithm-----
+
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int cursum=nums[0];
+        int maxsum=nums[0];
+        for(int i=1;i<nums.length;i++){
+            cursum=Math.max(cursum+nums[i],nums[i]);
+            maxsum=Math.max(cursum,maxsum);
+        }
+        return maxsum;
+    }
+}
+
+
 /*
 	53. Maximum Subarray
 

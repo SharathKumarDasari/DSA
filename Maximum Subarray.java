8b63ce9234663cb0a362df270627f8d48e7a250b
sharath <sharathdasari1719@gmail.com>
2026-03-04 16:53:05 +0530

    Maximum Subarray using dp

---
+++ Maximum Subarray.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        
+        int dp[]=new int[nums.length];
+        dp[0]=nums[0];
+        int max=dp[0];
+        for(int i=1;i<nums.length;i++){
+            dp[i]=Math.max(nums[i],nums[i]+dp[i-1]);
+            max=Math.max(dp[i],max);
+        }
+        return max;
+    }
+}
+
+/*
+	53. Maximum Subarray
+
+Given an integer array nums, find the subarray with the largest sum, and return its sum.
+
+ 
+
+Example 1:
+
+Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+Output: 6
+Explanation: The subarray [4,-1,2,1] has the largest sum 6.
+Example 2:
+
+Input: nums = [1]
+Output: 1
+Explanation: The subarray [1] has the largest sum 1.
+Example 3:
+
+Input: nums = [5,4,-1,7,8]
+Output: 23
+Explanation: The subarray [5,4,-1,7,8] has the largest sum 23.
+ 
+
+Constraints:
+
+1 <= nums.length <= 105
+-104 <= nums[i] <= 104
+*/
